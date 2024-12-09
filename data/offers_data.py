f3b92685ada7edfab7690e1b6d5d54e95538211e
Gerry Baird <gerry.baird@uk.ibm.com>
2024-12-09 18:11:24 +0000

    More offers

--- data/offers_data.py
+++ data/offers_data.py
@@ -6,6 +6,10 @@ offers = [
     "ACME are offering discounted gym memberships for customers that renew their health insurance.",
     "New and existing customers of A-Star are being offered free retirement planning consultations with a qualified financial planner.",
     "A-Star is offering a free Apple watch for new customers taking out a personal injury policy",
-    "ACME Insurance is offering a 30% discount on their gold level travel insurance for customers with existing conditions."
+    "ACME Insurance is offering a 30% discount on their gold level travel insurance for customers with existing conditions.",
+    "ACME Insurance is offering a 10% discount on their platinum level travel insurance for customers participating in extreme sports.",
+    "A-Star is offering a free Apple watch for new customers taking out a extreme sports cover",
+    "Panda insurance is offering upgraded winter sports cover for existing customers renewing world wide travel insurance.",
+    "Panda insurance is offering 18 months cover for the price of 12 months for new customers taking world wide travel insurance."
 ]
 
