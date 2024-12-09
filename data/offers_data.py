dc9ba05ad6d06b3ab9a4057580d4e46e06be5bea
Gerry Baird <gerry.baird@uk.ibm.com>
2024-12-09 15:54:33 +0000

    Gold travel insurance offer

--- data/offers_data.py
+++ data/offers_data.py
@@ -5,6 +5,7 @@ offers = [
     "Customers renewing their travel insurance with Explorer's insurance are eligible for a 5% discount.",
     "ACME are offering discounted gym memberships for customers that renew their health insurance.",
     "New and existing customers of A-Star are being offered free retirement planning consultations with a qualified financial planner.",
-    "A-Star is offering a free Apple watch for new customers taking out a personal injury policy"
+    "A-Star is offering a free Apple watch for new customers taking out a personal injury policy",
+    "ACME Insurance is offering a 30% discount on their gold level travel insurance for customers with existing conditions."
 ]
 
