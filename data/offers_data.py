2298bcd8467a61c5024f0090210139594f51c368
Gerry Baird <gerry.baird@uk.ibm.com>
2024-12-09 15:02:11 +0000

    extended offers

--- data/offers_data.py
+++ data/offers_data.py
@@ -4,6 +4,7 @@ offers = [
     "ACME Insurance is offering four complimentary passes to airport lounges for customers taking out a new travel insurance policy.",
     "Customers renewing their travel insurance with Explorer's insurance are eligible for a 5% discount.",
     "ACME are offering discounted gym memberships for customers that renew their health insurance.",
-    "New and existing customers of A-Star are being offered free retirement planning consultations with a qualified financial planner."
+    "New and existing customers of A-Star are being offered free retirement planning consultations with a qualified financial planner.",
+    "A-Star is offering a free Apple watch for new customers taking out a personal injury policy"
 ]
 
