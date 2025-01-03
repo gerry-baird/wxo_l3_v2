320859d4ace0e76a90889bb9d1fc6bf57fb5536e
Gerry Baird <gerry.baird@uk.ibm.com>
2025-01-03 14:10:45 +0000

    Updated offers

--- data/offers_data.py
+++ data/offers_data.py
@@ -3,11 +3,12 @@
 offers = [
     "ACME Insurance is offering four complimentary passes to airport lounges for customers taking out a new travel insurance policy.",
     "Customers renewing their travel insurance with Explorer's insurance are eligible for a 5% discount.",
-    "ACME are offering discounted gym memberships for customers that renew their health insurance.",
-    "New and existing customers of A-Star are being offered free retirement planning consultations with a qualified financial planner.",
+    "ACME are offering discounted gym memberships for customers that renew their medical insurance.",
     "A-Star is offering a free Apple watch for new customers taking out a personal injury policy",
+    "ACME Insurance is offering a 5% discount on their silver level travel insurance for customers taking out a new travel insurance policy.",
+    "ACME Insurance is offering a 10% discount on their silver level travel insurance for customers renewing their travel insurance policy.",
     "ACME Insurance is offering a 30% discount on their gold level travel insurance for customers with existing conditions.",
-    "ACME Insurance is offering a 10% discount on their platinum level travel insurance for customers participating in extreme sports.",
+    "ACME Insurance is offering a 10% discount on their platinum level travel insurance for customers requiring extreme sports cover.",
     "A-Star is offering a free Apple watch for new customers taking out a extreme sports cover",
     "Panda insurance is offering upgraded winter sports cover for existing customers renewing world wide travel insurance.",
     "Panda insurance is offering 18 months cover for the price of 12 months for new customers taking world wide travel insurance."
